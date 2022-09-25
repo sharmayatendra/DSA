d44d61d969ab56d8afebf267a61355962acf3201
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-09-26 01:46:42 +0530

    anagram using freqCounter

--- freqCounter.js
+++ freqCounter.js
@@ -37,5 +37,69 @@ const same1 = (arr1, arr2) => {
 
 // this is not the optimised approach since it has Time complexity of: O(n**2)
 
-console.log(same([1, 2, 3, 1], [4, 1, 9, 1]));
-console.log(same1([1, 2, 3, 1], [4, 1, 9, 1]));
+// console.log(same([1, 2, 3, 1], [4, 1, 9, 1]));
+// console.log(same1([1, 2, 3, 1], [4, 1, 9, 1]));
+
+// Frequency Counter approach
+
+const freqCounter = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let freqCounter1 = {};
+  let freqCounter2 = {};
+
+  for (let val of arr1) {
+    freqCounter1[val] = (freqCounter1[val] || 0) + 1;
+    // console.log(":::", freqCounter1[val]);
+  }
+
+  for (let val of arr2) {
+    freqCounter2[val] = (freqCounter2[val] || 0) + 1;
+  }
+
+  for (let key in freqCounter1) {
+    if (!(key ** 2 in freqCounter2)) {
+      return false;
+    }
+    if (freqCounter2[key ** 2] !== freqCounter1[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(freqCounter([1, 2, 3, 1, 5], [4, 1, 9, 1, 2]));
+
+// valid Anagram problem: Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+const validAnagram = (str1, str2) => {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  let wordCountStr1 = {};
+  let wordCountStr2 = {};
+  for (let val of str1) {
+    wordCountStr1[val] = (wordCountStr1[val] || 0) + 1;
+    // console.log(":::", val);
+  }
+  for (let val of str2) {
+    wordCountStr2[val] = (wordCountStr2[val] || 0) + 1;
+  }
+
+  for (let key in wordCountStr1) {
+    if (wordCountStr2[key] !== wordCountStr1[key]) {
+      return false;
+    }
+    if (!(key in wordCountStr2)) {
+      return false;
+    }
+    // console.log(wordCountStr2[key]);
+  }
+  console.log(wordCountStr1);
+  console.log(wordCountStr2);
+  return true;
+};
+
+console.log(validAnagram("care", "race"));
