071e2432fbd8d3933a990e892acd841db972e15c
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-12-01 18:02:48 +0530

    instance & static methos

--- classConst.js
+++ classConst.js
@@ -5,6 +5,12 @@ class Student {
     this.firstName = firstName;
     this.lastName = lastName;
   }
+  getFullName() {
+    return `the full name is ${this.firstName} ${this.lastName}`;
+  }
+  static getTotal() {
+    return "total number of student are 10";
+  }
 }
 
 // new keyword is used to create the instance of defined class
@@ -13,3 +19,10 @@ let firstPerson = new Student("foo", "bar");
 
 console.log(firstPerson.firstName);
 console.log(firstPerson);
+
+// instance method: used to create method on every instances of class.
+console.log(firstPerson.getFullName());
+
+// static method: they are not created on each instance but on the main Object created using the class
+
+console.log(Student.getTotal());
