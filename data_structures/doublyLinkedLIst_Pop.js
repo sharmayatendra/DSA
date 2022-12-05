e70b3a16fc75c75147d1fec75ef69a3b9b321903
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-12-06 03:14:08 +0530

    doublyLinkedList pop implementation

---
+++ data_structures/doublyLinkedLIst_Pop.js
@@ -0,0 +1,45 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+// pop the node at the end of linkedList
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) return null;
+    let popNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = popNode.prev;
+      this.tail.next = null;
+      popNode.prev = null;
+    }
+    this.length--;
+    return popNode;
+  }
+}
