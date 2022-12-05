df78e684257b38e86d8e4c5b9f37721dcca112d5
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-12-06 02:39:36 +0530

    doublyLinkedList push implementation

---
+++ data_structures/doublyLinkedList_Push.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+// add new node at the end of linkedList
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
