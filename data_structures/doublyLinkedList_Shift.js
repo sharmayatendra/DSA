639c7cd64168a082cdd9cae6a6a2edd0b34bd3a6
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-12-06 04:44:56 +0530

    doublyLinkedList shift operation

---
+++ data_structures/doublyLinkedList_Shift.js
@@ -0,0 +1,60 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+// pop the node at the end of linkedList
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) return null;
+    let popNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = popNode.prev;
+      this.tail.next = null;
+      popNode.prev = null;
+    }
+    this.length--;
+    return popNode;
+  }
+
+  shift() {
+    if (this.length === 0) return null;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+}
