54d0d41aa77e4f8a56f3cd46446297d1295f9dc6
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-12-13 04:23:38 +0530

    graph addVertex solution

---
+++ data_structures/graph_addVertex.js
@@ -0,0 +1,11 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+}
+
+let g = new Graph();
