4bba5602d2b1aa7f4a4b6f2679440f537ea826cb
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-12-13 14:18:18 +0530

    addEdge graph implementation

--- data_structures/graph_addVertex.js
+++ data_structures/graph_addVertex.js
@@ -6,6 +6,14 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
 }
 
 let g = new Graph();
+g.addVertex("Noida");
+g.addVertex("Delhi");
+g.addVertex("Gurugram");
