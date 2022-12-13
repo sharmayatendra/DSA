658350eb775484707afd7ca7b9ef45b17ebe4993
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-12-13 15:02:46 +0530

    graph removeVertex implementation

--- data_structures/graph_addVertex.js
+++ data_structures/graph_addVertex.js
@@ -16,11 +16,28 @@ class Graph {
     this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
     this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
   }
+
+  removeVertex(v) {
+    for (let i = this.adjacencyList[v].length; i >= 0; i--) {
+      const adjacentVertex = this.adjacencyList[v].pop();
+      this.removeEdge(v, adjacentVertex);
+    }
+    // while (this.adjacencyList[v].length) {
+    //   const adjacentVertex = this.adjacencyList[v].pop();
+    //   this.removeEdge(v, adjacentVertex);
+    // }
+    delete this.adjacencyList[v];
+  }
 }
 
 let g = new Graph();
 g.addVertex("Noida");
 g.addVertex("Delhi");
 g.addVertex("Gurugram");
+g.addVertex("Jaipur");
 g.addEdge("Noida", "Gurugram");
 g.addEdge("Noida", "Delhi");
+g.addEdge("Noida", "Jaipur");
+g.addEdge("Delhi", "Jaipur");
+g.addEdge("Gurugram", "Jaipur");
+g.removeVertex("Jaipur");
