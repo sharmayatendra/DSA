427debb7017522c742e7294ce6168496f82936b3
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-12-23 23:03:40 +0530

    LC problem containsDuplicate2

---
+++ leetcode/easy/containsDuplicate2.js
@@ -0,0 +1,9 @@
+/**problem: 219
+ * Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+ * Input: nums = [1,2,3,1], k = 3
+Output: true
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+ */
+
+const containsNearByDuplicate = () => {};
