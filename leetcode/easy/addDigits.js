f762428c56346d9de545dac8160878ec87bbd21b
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-12-28 21:07:05 +0530

    addDIgits improved solution

--- leetcode/easy/addDigits.js
+++ leetcode/easy/addDigits.js
@@ -2,6 +2,7 @@
  * Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
  */
 
+// using recursion:
 var addDigits = function (num) {
   let sum = 0;
   while (num > 0) {
@@ -12,3 +13,12 @@ var addDigits = function (num) {
   if (sum >= 10) return addDigits(sum);
   else return sum;
 };
+
+// TC: O(1)
+const addDigits1 = function (num) {
+  if (num === 0) return num;
+  if (num <= 9) return num;
+  else return num % 9;
+};
+console.log(addDigits(38));
+console.log(addDigits1(38));
