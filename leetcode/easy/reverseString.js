bbe1b7474d83a766e75caeb107102cd81088c2be
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2023-01-21 03:03:04 +0530

    reverseStr LC problem

---
+++ leetcode/easy/reverseString.js
@@ -0,0 +1,28 @@
+/**
+ * problem: 344
+ * Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+ */
+
+const reverseStr = (s) => {
+  let i = 0;
+  let j = s.length - 1;
+  while (j > i) {
+    let temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+    i++;
+    j--;
+  }
+  return s;
+};
